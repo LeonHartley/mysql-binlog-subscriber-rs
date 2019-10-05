01c29e6bf53596defb5f9848ab6287a46479d64d
Leon Hartley <leon.hartley@degree53.com>
2019-10-05 21:57:10 +0100

    remove unused imports

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,12 @@ pub mod options;
 pub mod client;
 
 mod test {
-    use std::net::TcpStream;
-    use std::io::Read;
-
-    use super::protocol::{auth::Ok, buffer::Buffer, buffer::reader::BufferReader, command::binlog::DumpBinaryLog, event::EventHeader};
-    use super::io::stream::read_response;
-    use super::protocol::error::MySqlErr;
     use super::io::stream::MySqlClientStream;
     use super::query::{QueryResult, MasterStatus};
-    use super::protocol::response::{Response};
     use super::options::MySqlOptions;
     use super::builder::{MySqlClientBuilder, MySqlConnectResult};
 
     pub fn connect() {
-        let mut binlog_connected: bool = false;
-
         let builder = MySqlClientBuilder::new(MySqlOptions {
             host: "localhost:3306".into(),
             username: "user".into(),
